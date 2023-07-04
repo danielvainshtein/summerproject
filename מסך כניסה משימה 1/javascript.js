632cfc13f6407d4f121b18a2d88ae40727c56657
amitkedmi <amitkedmi98@gmail.com>
2023-07-04 15:18:57 +0300

    update html

--- מסך כניסה משימה 1/javascript.js
+++ מסך כניסה משימה 1/javascript.js
@@ -1,32 +1,73 @@
-function checkEnglish(txt){
-    for (let i = 0; i < txt.length; i++){
-        if(txt.charArt(i) >= "A" && txt.charArt(i) <= "Z"){
+function checkEnglish(txt) {
+    for (let i = 0; i < txt.length; i++) {
+        if (!(txt.charAt(i) >= "A" && txt.charAt(i) <= "Z")){
             return false;
         }
-    { 
-        return true;
-    }
+        {
+            return true;
+        }
     }
 }
 
 function clickButton() {
     //בדיקת איימיל
     const email = document.getElementById("email").value;
-
     if (email.indexOf("@") == -1) {
         alert("הכנס אימייל תקין ");
+        return false;
     }
-    {
-        if (email.indexOf(".") == -1) {
-            alert("הכנס אימייל תקין ");
 
-        }
+    if (email.indexOf(".") == -1) {
+        alert("הכנס אימייל תקין ");
+        return false;
     }
+    if (email.indexOf("@") != email.lastIndexOf("@")){
+        alert("אסור יותר משטורדל אחד");
+        return false;
+    }
+    
+    if (checkEnglish(email) == false) {
+        alert("הכנס איימיל רק באנגלית");
+        return false;
+    }
+
+    //שם משתמש רק באנגלית
+    
+    const username = document.getElementById("username").value
+    /*
+    if (checkEnglish(username) == false) {
+        alert("הכנס שם משתמש רק באנגלית");
+        return false;
+    }
+*/
+    //מינימום 4 תויים לשם משתמש
+    if(username.length < 4){
+        alert("הכנס שם משתמש יותר מ-4 תווים");
+        return false;
+    }
+    //משתמש קיים במערכת
+    
+
+    //שם פרטי מינימום 2 אותיות
+    const first_name = document.getElementById("name").value;
+    if (first_name.length < 2){
+        alert("הכנס שם פרטי יותר מ-2 תווים");
+        return false;
+    }
+
+    //שם משפחה מינימום 2 אותיות
+    const last_name = document.getElementById("lastname").value;
+    if (last_name.length < 2){
+        alert("הכנס שם משפחה יותר מ-2 תווים");
+        return false;
+    }
+    //מספר גיל בין 18-65
+    const age = document.getElementById("age").value;
+    if(age < 18 || age > 65){
+        alert("הגיל חייב להיות בין 18-65");
+        return false;
+    } 
+       
+    
 }
-
-//שם משתמש רק באנגלית
-
-
-
-
 document.getElementById("register").addEventListener("click", clickButton);
