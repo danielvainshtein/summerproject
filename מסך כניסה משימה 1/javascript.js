473828c3a9d58191d4a66d73ebd24d9cc35f2215
danielvainshtein <danielvainshtein2020@gmail.com>
2023-07-10 16:20:21 +0300

    password check

--- מסך כניסה משימה 1/javascript.js
+++ מסך כניסה משימה 1/javascript.js
@@ -67,6 +67,23 @@ function clickButton() {
         alert("הגיל חייב להיות בין 18-65");
         return false;
     } 
+
+    const pass1 = document.getElementById("password").value;
+    if(pass1 == ""){
+        alert("חובה למלא סיסמה");
+        return false;
+    }
+
+    const pass2 = document.getElementById("password2").value;
+    if (pass2 == ""){
+        alert("חובה למלא אימות סיסמה");
+        return false;
+    }
+
+    if (pass1 != pass2){
+        alert("אימות סיסמה שגוי");
+        return false;
+    }
        
     
 }
