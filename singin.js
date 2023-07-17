805ced961c1cd70409a14bb0c86ab86eb0eaadb3
amitkedmi <amitkedmi98@gmail.com>
2023-07-17 16:20:20 +0300

    page singin

--- singin.js
+++ singin.js
@@ -1,5 +1,21 @@
+//קישור בין הlocal storge לדף singin
 const users = JSON.parse(localStorage.getItem("users")) || [];
 
+const username = document.getElementById("username").value;
+const password = document.getElementById("Password").value;
+    const index = users.findIndex(function (u) {
+        return username == u.username;
+    })
+    if (index == -1 || password != users[index].password || chance < 2) {
+        alert("Wrong username or password!");
+    }
+
+
+    document.getElementById("submit").addEventListener("click", function () {
+    })
+
+
+/*
 const registeredUsernames = users.Map(u => u.username);
 
 const username = document.getElementById("username").Value;
