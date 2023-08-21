705638cac243616f1c38c50009d435a136755871
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-08-21 17:35:16 +0300

    make pincode worls

--- Reset_password.js
+++ Reset_password.js
@@ -1,25 +1,41 @@
+var number_random;
+const users = localStorage.getItem("data");
+const currentUser = localStorage.getItem("currentUser");
+const data = JSON.parse(users);
+const userIndex = data.findIndex((u) => u.username == currentUser);
 
+
+document.getElementById('check_pincode').addEventListener('click', function (e) {
+    e.preventDefault();
+    const Code_Compliant = document.getElementById("Code_Compliant").value;
+
+    console.log(Code_Compliant);
+    if (number_random == Code_Compliant) {
+
+
+        data[userIndex].password1 = document.getElementById("new_password").value;
+        window.localStorage.setItem('data', JSON.stringify(data));
+
+
+    }
+});
 
 document.getElementById("save_password").addEventListener('click', function (e) {
-e.preventDefault();
-var number_randon = (Math.floor(Math.random()*10000));
-console.log(number_randon);
+    e.preventDefault();
+    const [minNumber, maxNumber] = [1000, 10000];
 
-const Code_Compliant = document.getElementById("Code_Compliant").value;
+    number_random = minNumber + (Math.floor(Math.random() * (maxNumber - minNumber)));
 
-if(number_randon == Code_Compliant){
+    console.log(number_random);
+    if (data[userIndex].password1 != document.getElementById("Current_password").value) {
+        alert("Wrong pass")
+    } else {
+        document.getElementById('inputContainer').classList.remove('invisible');
 
-    const users = localStorage.getItem("data");
-    const currentUser = localStorage.getItem("currentUser");
-    const data = JSON.parse(users);
-    const user = data.find(u => u.username == currentUser)
+    }
 
-    const userIndex = data.findIndex((u) => u.username == currentUser);
-    data[userIndex].password1 = document.getElementById("new_password").value;
-    window.localStorage.setItem('data', JSON.stringify(data));
 
-    
-}
+
 })
 
 
